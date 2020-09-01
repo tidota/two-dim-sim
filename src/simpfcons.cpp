8d69bce6d1b684bb89b7b03b3f76f0239b6e7b69
Tetsuya Idota <tidota@hawaii.edu>
2020-09-01 01:15:57 -1000

    Speedup SimPfCons::evalByOmega

--- src/simpfcons.cpp
+++ src/simpfcons.cpp
@@ -17,15 +17,18 @@ SimPfCons::SimPfCons(const YAML::Node& doc): SimNonParam(doc),
 void SimPfCons::evalByOmega(
   const std::vector<VectorXd>& est, std::vector<double>& cumul_weights)
 {
-  const double sigma = 0.1;
+  const double sigma = 0.05;
   for (int i = 0; i < n_particles; ++i)
-  {
     cumul_weights[i] = 0;
+  for (int i = 0; i < n_particles - 1; ++i)
+  {
     // estimate the value on the point of the probability distribution
-    for (int j = 0; j < n_particles; ++j)
+    for (int j = i + 1; j < n_particles; ++j)
     {
       VectorXd diff = est[i] - est[j];
-      cumul_weights[i] += exp(-diff.squaredNorm()/sigma/sigma);
+      double val = exp(-diff.squaredNorm()/sigma/sigma);
+      cumul_weights[i] += val;
+      cumul_weights[j] += val;
     }
     cumul_weights[i] = pow(cumul_weights[i], mode6_omega - 1);
     if (i > 0)
@@ -106,16 +109,16 @@ void SimPfCons::mutualLocImpl(const VectorXd& z, const std::pair<int,int>& edge)
   const int r2 = edge.second;
 
   // calcualte cumulative confidence reduction weights for each population
-  std::vector<double> cumul_weights_omega1(n_particles, 0);
+  std::vector<double> cumul_weights_omega1(n_particles);
   evalByOmega(ests[r1], cumul_weights_omega1);
 
-  std::vector<double> cumul_weights_omega2(n_particles, 0);
+  std::vector<double> cumul_weights_omega2(n_particles);
   evalByOmega(ests[r2], cumul_weights_omega2);
 
   if (enable_bidirectional)
   {
     // calculate cumulative mutual measurement weights for each population
-    std::vector<double> cumul_weights1(n_particles, 0);
+    std::vector<double> cumul_weights1(n_particles);
     evalByZ(
       ests[r1], cumul_weights_omega1, ests[r2], cumul_weights_omega2,
       cumul_weights1, -z);
@@ -124,7 +127,7 @@ void SimPfCons::mutualLocImpl(const VectorXd& z, const std::pair<int,int>& edge)
   }
 
   // calculate mutual measurement weights for each population
-  std::vector<double> cumul_weights2(n_particles, 0);
+  std::vector<double> cumul_weights2(n_particles);
   evalByZ(
     ests[r2], cumul_weights_omega2, ests[r1], cumul_weights_omega1,
     cumul_weights2, z);
